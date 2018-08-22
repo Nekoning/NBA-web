f4a1c8b1edf8f7a84a9e1ea22bf341ac96392c51
Nekoning <nekoningwang@gmail.com>
2018-08-22 00:35:49 -0400

    nba profile init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import nba from 'nba';
+import { ShotChart } from './ShotChart';
 import { Profile } from "./Profile";
-import { ShotChart } from "./ShotChart";
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer("Stephen Curry").playerId,
-        playerInfo: {}
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
     }
 
     componentDidMount() {
-        const playerId = nba.findPlayer("Stephen Curry").playerId;
-        nba.stats.playerInfo({ PlayerID: playerId }).then((response) => {
-            const playerInfo = Object.assign({}, response.commonPlayerInfo[0], response.playerHeadlineStats[0]);
-
+        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playerInfo);
             this.setState({
-                playerInfo
+                playerInfo: playerInfo,
             });
-
-        })
-
+        });
     }
 
     render() {
         return (
             <div className="main">
-                <Profile />
+                <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
                 <ShotChart playerId={this.state.playerId}/>
             </div>
         );
