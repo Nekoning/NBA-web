c284116a19ccc06774a69ee51e1a1251d62b897a
Nekoning <nekoningwang@gmail.com>
2018-08-28 22:33:53 -0400

    fix search bar css style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
+import nba from 'nba';
 
 export class SearchBar extends React.Component{
+    state = {
+        dataSource: [],
+    }
+
+    handleSearch = (value) => {
+        this.setState({
+            dataSource: !value ? [] : nba.searchPlayers(value).map(
+                ({ fullName }) => fullName
+            ),
+        });
+    }
+
+    onSelect = (value) => {
+        console.log('onSelect', value);
+    }
+
     render() {
+        const { dataSource } = this.state;
         return (
-            <div>
-                Search Bar
-            </div>
+            <AutoComplete
+                className = "search-bar"
+                dataSource={dataSource}
+                size="large"
+                onSelect={onSelect}
+                onSearch={this.handleSearch}
+                placeholder="Search NBA player"
+            >
+                <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+            </AutoComplete>
         );
     }
 }
