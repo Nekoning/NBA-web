cce071dd33b12c5dfc4a8425a38014e24b34a5c3
Nekoning <nekoningwang@gmail.com>
2018-08-28 23:14:58 -0400

    searchbar dropdown css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AutoComplete, Input, Icon } from 'antd';
 import nba from 'nba';
+import {PROFILE_PIC_URL_PREFIX} from "../constants"
 
 const Option = AutoComplete.Option;
 
@@ -12,8 +13,11 @@ export class SearchBar extends React.Component{
     handleSearch = (value) => {
         this.setState({
             dataSource: !value ? [] : nba.searchPlayers(value).map(
-                ({ fullName, playerId }) => (<Option> key={ playerId } value={ fullName }
-                    { fullName }
+                ({ fullName, playerId }) => (<Option key={ playerId } value={ fullName }>
+                    <img className="player-option-image"
+                        src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`} alt="profile image"
+                    />
+                    <span className="player-option-label">{ fullName }</span>
                 </Option>)
             ),
         });
